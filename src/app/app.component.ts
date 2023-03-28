93df58f07179d9d3af3baf6a785d4731b246847b
Stephen Heinen <stephenheinen22@yahoo.com>
2023-03-28 03:31:24 -0500

    finished shopping list form and shopping service (i think)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'food-planning';
-  loadedFeature = 'recipe';
-
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 }
